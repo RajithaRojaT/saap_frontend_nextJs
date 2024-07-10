7b64cf83b1ffe220eaaaa63c57c4f8b0a9e5a1cd
RajithaRojaT <115389326+RajithaRojaT@users.noreply.github.com>
2024-07-10 15:47:04 +0530

    Add files via upload

---
+++ data/pricing/PricingPlansData.js
@@ -0,0 +1,25 @@
+
+
+export const multisite = [
+	{
+		plantitle: 'Elevate Your Learning Journey with AStarPrep Subscription',
+		description: `Welcome to AStarPrep, your personalized solution for growth and development. With our subscription service, you gain access to a wealth of resources designed to assess your current status, identify areas for improvement, and assist you in reaching your goals.`,
+		monthly: 1,
+		buttonText: 'Buy Subscription',
+		buttonClass: 'primary',
+		featureHeading: 'What You Get:',
+		features: [
+			{ feature: 'Comprehensive Assessments' },
+			{ feature: `Interactive Tools and Resources` },
+			{ feature: 'Customized Action Plans' },
+			{ feature: 'Expert Guidance' },
+			{ feature: 'Progress Tracking' }
+		]
+	}
+];
+
+
+
+export const PricingPlansData = [multisite];
+
+export default PricingPlansData;
